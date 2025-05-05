9f49773af883c374ca4964f1f66b453fa8d723f3
Ignacio <ignaciorassol@gmail.com>
2025-05-05 01:00:32 -0300

    Added navmesh plus dependency. Do some basic testing with tile map

--- Assets/Scripts/pokemon.cs
+++ Assets/Scripts/pokemon.cs
@@ -4,19 +4,18 @@ using UnityEngine.AI;
 public class MoverAgente : MonoBehaviour
 {
     public NavMeshAgent agent;
+    public Camera cam;
 
+    void Start()	{
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 click = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            click.y = 0;
+            Vector3 click = cam.ScreenToWorldPoint(Input.mousePosition);
             agent.SetDestination(click);
         }
-
-        // Mantener en Z=0 si es 2D
-        Vector3 pos = agent.transform.position;
-        pos.y = 0;
-        agent.transform.position = pos;
     }
 }
