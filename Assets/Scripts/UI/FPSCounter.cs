84d02c2ebd2bdfb16f0a29d25dc689f0dc20fe9a
David <141585041+David-Uzno@users.noreply.github.com>
2025-08-29 03:32:47 -0300

    feat: limitador de FPS

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _textFPS;
+    float _elapsedTime = 0.5f;
+
+    [Tooltip("Número de decimales. Utilización de un Start(), no se actualizará frame a frame.")]
+    [SerializeField, Range(0, 5)] int _decimals = 0;
+    string _decimalString;
+
+    void Start()
+    {
+        _decimalString = "F" + _decimals;
+    }
+
+    void Update()
+    {
+        if (_textFPS.gameObject.activeSelf)
+        {
+            _elapsedTime += Time.unscaledDeltaTime;
+
+            if (_elapsedTime >= 0.5f)
+            {
+                // Calcular FPS Promedio
+                float FPSValue = 1f / Time.unscaledDeltaTime;
+
+                // Actualizar el Texto de FPS
+                _textFPS.text = "FPS " + FPSValue.ToString(_decimalString);
+                
+                _elapsedTime = 0f;
+            }
+        }
+    }
+
+    public void Show()
+    {
+        _textFPS.gameObject.SetActive(!_textFPS.gameObject.activeSelf);
+    }
+}
